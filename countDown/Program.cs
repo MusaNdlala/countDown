1665402920cb4adf6e931e0552f6d8c7ae286a19
Musa <Musa@DESKTOP-N08JB8A>
2021-09-10 14:55:43 +0200

    Add project files.

---
+++ countDown/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace countDown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Countdown(10,1);
+            Console.Read();
+        }
+        static void Countdown(Int16 intStartNum, Int16 intEndNum)
+        {
+            
+            do
+            {
+                Console.WriteLine(intStartNum--);
+            } while (intStartNum >= intEndNum);
+
+        }
+    }
+}
